9cc9dcef73ed39aaf33ce8989fc7db538000b5a8
Jagadishwar Sanjeevraopet <151638069+SJagadishwar@users.noreply.github.com>
2026-02-08 11:05:08 +0530

    Update dashboard to show adoption rate and engagement intensity

--- admin/analytics.js
+++ admin/analytics.js
@@ -2,11 +2,13 @@ fetch("/analytics/summary")
   .then(res => res.json())
   .then(data => {
     document.getElementById("sessions").innerText = data.totalSessions;
-    document.getElementById("usage").innerText = data.usageRate + "%";
+    document.getElementById("usage").innerText = data.adoptionRate + "%";
     document.getElementById("returned").innerText = data.returnedToOriginal;
 
     document.getElementById("hinglish").innerText = data.languages.hinglish;
     document.getElementById("telgish").innerText = data.languages.telgish;
+    document.getElementById("intensity").innerText = data.engagementIntensity;
+
 
     document.getElementById("before").innerText = data.avgTimeBefore;
     document.getElementById("after").innerText = data.avgTimeAfter;
